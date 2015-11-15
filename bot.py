535e60a1792be70a5946aabad29b776eda1046cd
Leo <kjungjoon@gmail.com>
2015-11-14 22:52:36 -0500

    started part 2

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import praw
 
-user_agent = ("Advisor Bot")
+user_agent = ("Advisor Bot 0.1")
 
 r = praw.Reddit(user_agent = user_agent)
 
-subreddit = r.get_subreddit("uwaterloo")
+subreddit = r.get_subreddit("pythonforengineers")
 
-for submission in subreddit.get_new(limit = 5):
+for submission in subreddit.get_hot(limit = 5):
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
